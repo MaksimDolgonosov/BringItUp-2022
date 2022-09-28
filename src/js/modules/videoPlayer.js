dde054053099e9d834becc7dec3f7148c25011af
Malsim Dauhanosav <max_air@bk.ru>
2022-09-28 20:32:47 +0300

    Динамические плееры. Готово

--- src/js/modules/videoPlayer.js
+++ src/js/modules/videoPlayer.js
@@ -5,6 +5,7 @@ export default class VideoPlayer {
         this.closeBtn = this.overlay.querySelector(`.close`);
 
         this.closeBtn.addEventListener("click", () => {
+            // this.player.stopVideo();
             this.overlay.style.display = "none";
             document.querySelector("#frame").remove();
             let div = document.createElement("div");
@@ -14,23 +15,53 @@ export default class VideoPlayer {
     }
 
 
-
     createPlayer(url) {
         this.player = new YT.Player('frame', {
             height: '100%',
             width: '100%',
-            videoId: `${url}`
+            videoId: `${url}`,
+            events: {
+                'onStateChange': this.onPlayerStateChange.bind(this) // не забыть
+            }
         });
         this.overlay.style.display = "flex";
-
     }
+
+    onPlayerStateChange(event) {
+
+        try {
+            let blockedBlock = this.activeTriger.closest(".module__video-item").nextElementSibling;
+            if (event.data == 0) {
+                if (blockedBlock.querySelector(".play__circle").classList.contains("closed")) {
+                    blockedBlock.style.opacity = "1";
+                    blockedBlock.style.filter = "none";
+                    blockedBlock.querySelector(".play__circle").classList.remove("closed");
+                    blockedBlock.querySelector(".play__text").classList.remove("attention");
+                    blockedBlock.querySelector(".play__circle svg").remove();
+                    blockedBlock.querySelector(".play__circle").appendChild(this.activeSvg);
+                    blockedBlock.querySelector(".play__text").textContent = "play video";
+                }
+            }
+        } catch (e) { }
+    }
+
 
     init() {
         this.triggers.forEach(trigger => {
             trigger.addEventListener("click", () => {
-                let path = trigger.getAttribute("data-url");
-                this.createPlayer(path);
+
+                if (trigger.querySelector(".play__circle").classList.contains("closed")) {
+
+                } else {
+                    this.activeTriger = trigger;
+                    this.activeSvg = trigger.querySelector("svg").cloneNode(true);
+                    let path = trigger.getAttribute("data-url");
+                    this.createPlayer(path);
+                }
+
             });
         });
+
+
     }
 }
